26cac6b4e1b16b4b161402d9f2472549ca65e62f
Nicola Parolini <nicola.parolini@polimi.it>
2022-05-04 09:51:57 +0200

    update

--- blog_postproc.py
+++ blog_postproc.py
@@ -11,6 +11,7 @@ print(data['deceduti'].diff())
 data['Deceduti giornalieri'] = np.concatenate([data['deceduti'].diff()])
 
 comp = ['Nuovi positivi', 'Positivi', 'Ricoverati (non in terapia intensiva)', 'Ricoverati in terapia intensiva',  'Deceduti giornalieri']
+label = ['Casi giornalieri', 'Positivi', 'Ricoverati in reparti ordinari', 'Ricoverati in terapia intensiva',  'Deceduti giornalieri']
 comp2 = ['nuovi_positivi', 'totale_positivi', 'ricoverati_con_sintomi', 'terapia_intensiva',  'Deceduti giornalieri']
 
 table = pd.DataFrame(columns=['Compartment','Past week','Next week','trend','Next week 0.025','Next week 0.975'])
@@ -22,13 +23,14 @@ for i in range(5):
         trend = '&uarr;'
     else:
         trend = '&darr;'
-    table = table.append({'Compartment': comp[i], 
+    table = table.append({'Compartment': label[i], 
                           'Past week': int(data[comp2[i]].iloc[-7:].mean()),
                           'Next week': int(forecast[comp[i]].iloc[1:8].mean()),
                           'trend': trend,
                           'Next week 0.025': int(forecastL[comp[i]].iloc[1:8].mean()),
                           'Next week 0.975': int(forecastU[comp[i]].iloc[1:8].mean())}, ignore_index=True)
     #print(comp[i],int(data[comp2[i]].iloc[-7:].mean()),int(forecast[comp[i]].iloc[1:8].mean()),int(forecastL[comp[i]].iloc[1:8].mean()),int(forecastU[comp[i]].iloc[1:8].mean()))
+
 
 table.to_csv('data.csv')
 
